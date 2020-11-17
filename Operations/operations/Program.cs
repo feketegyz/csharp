252ea1f50a7dc82f531ed31b7162c48168a8e483
Fekete <Fekete@HP15S-20>
2020-11-17 22:42:31 +0100

    Alapműveletek beemelve

---
+++ Operations/operations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alapműveletek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alapmüvelet gyakorlás");
+
+            Random r = new Random();
+            int[] szam1 = new int[10];
+            int[] szam2 = new int[10];
+            int[] szum = new int[10];
+            int jo = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                szam1[i] = r.Next(0, 100);
+                szam2[i] = r.Next(0, 100);
+
+                Console.Write(szam1[i] + "+" + szam2[i] + "= ");
+                szum[i] = int.Parse(Console.ReadLine());
+                //Console.Write(szam1[i]+szam2[i]);
+                if (szam1[i] + szam2[i] == szum[i])
+                {
+                    //Console.WriteLine("  helyes");
+                    jo++;
+                }
+
+            }
+            //for(int i = 0; i < 9; i++)
+
+            Console.WriteLine($"10-ből " + jo + " jó");
+            Console.ReadKey();
+        }
+    }
+}
